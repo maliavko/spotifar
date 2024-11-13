3d292515a34eff1363e382050cf456c26b44ddbd
igor maliavko <maliavko@gmail.com>
2024-11-13 21:31:05 +0100

    adapting build.bat to work best with vcpkg as a git submodule

--- build.bat
+++ build.bat
@@ -1,15 +1,9 @@
 @echo off
 
+git submodule update --init
+
 :::: Preparing a build folder
 if not exist build mkdir build
 
-:::: Preparing a toolchain
-if not exist vcpkg (
-	git clone https://github.com/microsoft/vcpkg.git
-	cd vcpkg && ./bootstrap-vcpkg.bat
-	cd ..
-)
-
-cd ./build
-cmake ..
+cd ./build && cmake ..
 cmake --build . --config Debug
