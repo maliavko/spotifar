4ca1b4f07e82a850c1ecb64014dfac1f924d9adc
igor maliavko <maliavko@gmail.com>
2024-11-15 11:22:39 +0100

    changing thirdparties folder structure; adding script to bring and build far for testing purposes

---
+++ thirdparty/farmanager/build-vs2022-debug-x64.bat
@@ -0,0 +1,5 @@
+
+git clone --depth 1 https://github.com/FarGroup/FarManager build
+
+cd build/far/
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" & build.bat vc 64 debug
