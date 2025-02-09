d51573055e394f756068bb5e68dcabbe66ad6af6
igor maliavko <maliavko@gmail.com>
2025-02-09 21:09:10 +0100

    test with updating playback ui from the main thread

--- src/spotify/playback.cpp
+++ src/spotify/playback.cpp
@@ -20,36 +20,40 @@ clock_t::duration playback_cache::get_sync_interval() const
     return 950ms;
 }
 
+
+template <class P, typename... MethodArgumentTypes, typename... ActualArgumentTypes>
+static void dispatch_event(void (P::*method)(MethodArgumentTypes...), ActualArgumentTypes... args)
+{
+    utils::far3::synchro_tasks::push([method, args...] {
+        ObserverManager::notify(method, args...);
+    });
+}
+
 void playback_cache::on_data_synced(const playback_state &data, const playback_state &prev_data)
 {
     if (data.item != prev_data.item)
-        ObserverManager::notify(&playback_observer::on_track_changed, data.item);
+        dispatch_event(&playback_observer::on_track_changed, data.item);
 
     if (data.progress_ms != prev_data.progress_ms)
-        ObserverManager::notify(&playback_observer::on_track_progress_changed,
-                                data.item.duration, data.progress);
+        dispatch_event(&playback_observer::on_track_progress_changed, data.item.duration, data.progress);
 
     if (data.device.volume_percent != prev_data.device.volume_percent)
-        ObserverManager::notify(&playback_observer::on_volume_changed,
-                                data.device.volume_percent);
+        dispatch_event(&playback_observer::on_volume_changed, data.device.volume_percent);
 
     if (data.shuffle_state != prev_data.shuffle_state)
-        ObserverManager::notify(&playback_observer::on_shuffle_state_changed,
-                                data.shuffle_state);
+        dispatch_event(&playback_observer::on_shuffle_state_changed, data.shuffle_state);
 
     if (data.repeat_state != prev_data.repeat_state)
-        ObserverManager::notify(&playback_observer::on_repeat_state_changed,
-                                data.repeat_state);
+        dispatch_event(&playback_observer::on_repeat_state_changed, data.repeat_state);
 
     if (data.is_playing != prev_data.is_playing)
-        ObserverManager::notify(&playback_observer::on_state_changed,
-                                data.is_playing);
+        dispatch_event(&playback_observer::on_state_changed, data.is_playing);
 
     if (data.context != prev_data.context)
-        ObserverManager::notify(&playback_observer::on_context_changed, data.context);
+        dispatch_event(&playback_observer::on_context_changed, data.context);
 
     if (data.actions != prev_data.actions)
-        ObserverManager::notify(&playback_observer::on_permissions_changed, data.actions);
+        dispatch_event(&playback_observer::on_permissions_changed, data.actions);
 
     // TODO: send changes in permissions
 }
