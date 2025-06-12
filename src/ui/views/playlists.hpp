b1310dfec0518811fea6bb550de2e8ee70dc2a25
IgorM <maliavko@gmail.com>
2025-06-12 19:48:57 +0200

    switched all the views to the new columns management mechanism

--- src/ui/views/playlists.hpp
+++ src/ui/views/playlists.hpp
@@ -27,10 +27,10 @@ protected:
     // view interface
     auto get_sort_modes() const -> const sort_modes_t& override;
     auto select_item(const data_item_t* data) -> intptr_t override;
-    void update_panel_info(OpenPanelInfo *info) override;
     auto compare_items(const sort_mode_t &sort_mode, const data_item_t *data1, const data_item_t *data2) -> intptr_t override;
     auto process_key_input(int combined_key) -> intptr_t override;
     bool request_extra_info(const data_item_t *data) override;
+    auto get_panel_modes() const -> const panel_modes_t* override;
 protected:
     api_weak_ptr_t api_proxy;
 };
