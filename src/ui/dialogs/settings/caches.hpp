b20425e6240aed02e7180ff96f8f86c3b646c60a
Igor M <maliavko@gmail.com>
2025-06-18 16:57:14 +0200

    adding draft of a new config subdialog - Caches&logs, unfinished

---
+++ src/ui/dialogs/settings/caches.hpp
@@ -0,0 +1,34 @@
+#ifndef CACHES_HPP_2FE1C7CC_0C2E_4D37_AA79_49114D82E549
+#define CACHES_HPP_2FE1C7CC_0C2E_4D37_AA79_49114D82E549
+#pragma once
+
+#include "stdafx.h"
+#include "ui/dialogs/dialog.hpp"
+#include "spotify/observer_protocols.hpp"
+
+namespace spotifar { namespace ui { namespace settings {
+
+class caches_dialog:
+    public modal_dialog,
+    public spotify::releases_observer
+{
+public:
+    caches_dialog();
+    ~caches_dialog();
+protected:
+    void set_releases_sync_status(size_t items_left = 0);
+
+    // modal_dialog
+    void init() override;
+    auto handle_result(intptr_t dialog_run_result) -> intptr_t override;
+    bool handle_btn_clicked(int ctrl_id) override;
+
+    // releases_observer handlers
+    void on_sync_progress_changed(size_t items_left) override;
+};
+
+} // namespace settings
+} // namespace ui
+} // namespace spotifar
+
+#endif // CACHES_HPP_2FE1C7CC_0C2E_4D37_AA79_49114D82E549
