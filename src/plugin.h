c77759842b76b7abc3fd20035fb3b62938d23869
Igor M <maliavko@gmail.com>
2025-06-29 00:43:43 +0200

    - feature, notifications can be called via global static functions from any source - feature, introduced hotkeys_handler with all the global hotkeys-related logic - refactor, librespot class is moved directly to the plugin class - refactor, removed playback_handler class as unneeded

--- src/plugin.h
+++ src/plugin.h
@@ -3,6 +3,8 @@
 #pragma
 
 #include "ui/events.hpp"
+#include "librespot.hpp"
+#include "hotkeys_handler.hpp"
 #include "spotify/observer_protocols.hpp"
 
 namespace spotifar {
@@ -23,9 +25,9 @@ class plugin:
     public plugin_interface,
     public config::config_observer,
     public spotify::auth_observer,
-    public spotify::releases_observer,
     public spotify::api_requests_observer,
-    public ui::ui_events_observer
+    public ui::ui_events_observer,
+    public librespot_observer
 {
 public:
     plugin();
@@ -36,19 +38,16 @@ public:
 protected:
     void launch_sync_worker();
     void shutdown_sync_worker();
-    
-    void process_win_messages_queue();
+
+    void launch_librespot_process(const string &access_token);
+    void shutdown_librespot_process();
 
     // config handlers
-    void on_global_hotkeys_setting_changed(bool is_enabled) override;
-    void on_global_hotkey_changed(config::settings::hotkeys_t changed_keys) override;
-    void on_logging_verbocity_changed(bool is_verbose) override;
+    void on_playback_backend_setting_changed(bool is_enabled) override;
+    void on_playback_backend_configuration_changed() override;
 
     // auth handler
     void on_auth_status_changed(const spotify::auth_t &auth, bool is_renewal) override;
-
-    // recent releases handler
-    void on_releases_sync_finished(const spotify::recent_releases_t releases) override;
     
     // ui events handlers
     void show_player() override;
@@ -58,18 +57,19 @@ protected:
     void on_request_progress_changed(const string &url, size_t progress, size_t total) override;
     void on_playback_command_failed(const string &msg) override;
     void on_collection_fetching_failed(const string &msg) override;
+
+    // librespot observer handlers
+    void on_librespot_stopped(bool emergency) override;
+    void on_librespot_discovered(const spotify::device_t &dev, const spotify::device_t &active_dev) override;
 private:
     std::mutex sync_worker_mutex; // is locked all the way until worker is active
     std::atomic<bool> is_worker_listening = false;
 
-    // a separate tasks queue, so the main thread can execute some code within
-    // background thread, like hotkeys check etc.
-    utils::tasks_queue background_tasks;
-
-    std::unique_ptr<ui::notifications> notifications;
+    std::unique_ptr<ui::notifications_handler> notifications;
     std::unique_ptr<ui::player> player;
+    std::unique_ptr<librespot> librespot;
+    std::unique_ptr<hotkeys_handler> hotkeys;
     std::shared_ptr<spotify::api> api;
-    playback_handler_ptr_t playback_handler;
 };
 
 } // namespace spotifar
--- src/ui/notifications.hpp
+++ src/ui/notifications.hpp
@@ -7,23 +7,49 @@
 
 namespace spotifar { namespace ui {
 
+namespace notifications
+{
+    /// @brief Shows a 'now-playing' pop-up with the current track info
+    /// @param show_buttons to show extra functional buttons
+    void show_now_playing(const spotify::track_t &track, bool show_buttons = false);
+
+    /// @brief Shows pop-up with the list of recent releases, which built-in
+    /// scan worker managed to find
+    void show_releases_found(const spotify::recent_releases_t &releases);
+}
+
+
+struct notifications_observer: public BaseObserverProtocol
+{
+    virtual void show_now_playing(const spotify::track_t &track, bool show_buttons = false) {}
+
+    virtual void show_releases_found(const spotify::recent_releases_t &releases) {}
+};
+
+
 /// @brief A class-helpers for showing app win toasts notifications in the
 /// windows tray notifications area
-class notifications:
-    public spotify::playback_observer   // for showing up the windows notification when track has changed
+class notifications_handler:
+    public spotify::playback_observer,   // for showing up the windows notification when track has changed
+    public spotify::releases_observer,
+    public notifications_observer
 {
 public:
-    notifications(spotify::api_weak_ptr_t api): api_proxy(api) {}
-    ~notifications() { api_proxy.reset(); }
+    notifications_handler(spotify::api_weak_ptr_t api): api_proxy(api) {}
+    ~notifications_handler() { api_proxy.reset(); }
 
     bool start();
     bool shutdown();
-    
-    void show_now_playing(const spotify::track_t &track, bool show_buttons = false);
-    void show_recent_releases_found(const spotify::recent_releases_t &releases);
 protected:
+    // notifications observer handlers
+    void show_now_playing(const spotify::track_t &track, bool show_buttons = false) override;
+    void show_releases_found(const spotify::recent_releases_t &releases) override;
+
     // playback handlers
     void on_track_changed(const spotify::track_t &track, const spotify::track_t &prev_track) override;
+
+    // recent releases handlers
+    void on_releases_sync_finished(const spotify::recent_releases_t releases) override;
 private:
     spotify::api_weak_ptr_t api_proxy;
 };
